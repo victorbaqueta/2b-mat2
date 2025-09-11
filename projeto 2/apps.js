829b943dea82059f8152950d35776658564c1193
victorbaqueta <victor.baqueta@escola.pr.gov.br>
2025-09-11 11:29:44 -0300

    .

--- projeto 2/apps.js
+++ projeto 2/apps.js
@@ -4,14 +4,15 @@ function criaCartao(categoria, pergunta, resposta) {
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
     cartao.innerHTML = `   
-     <div class="cartao__conteudo">
-                    <h3>Animais</h3>
-                    <div class="cartao__conteudo__pergunta">
-                        <p>Qual a principal diferença entre um Camelo e o Dromedário?</p>
-                    <div class="cartao__conteudo__resposta">
-                       <p>A principal diferença é o número de corcovas: o camelo tem duas corcovas, enquanto o dromedário tem apenas uma. </p>
-                    </div>
-                </div>
-                `
-                container.appendChild(cartao)
+    <div class="cartao__conteudo">
+        <h3>Animais</h3>
+        <div class="cartao__conteudo__pergunta">
+            <p>Qual a principal diferença entre um Camelo e o Dromedário?</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+            <p>A principal diferença é o número de corcovas: o camelo tem duas corcovas, enquanto o dromedário tem apenas uma. </p>
+        </div>
+    </div>
+    `
+    container.appendChild(cartao)
 }
--- projeto 2/perguntas.js
+++ projeto 2/perguntas.js
@@ -1,17 +1,17 @@
 criaCartao(
-   'animais'
-   'Qual a principal diferença entre um Camelo e o Dromedário?'
+   'animais',
+   'Qual a principal diferença entre um Camelo e o Dromedário?',
    'A principal diferença é o número de corcovas: o camelo tem duas corcovas, enquanto o dromedário tem apenas uma.'
 )
 
 criaCartao(
   'pessoas',
-   'Por que os idosos diminuem de tamanho?'
+   'Por que os idosos diminuem de tamanho?',
     'Os idosos perdem altura principalmente porque os discos intervertebrais da coluna se achatam e perdem fluidos, e a massa óssea e muscular diminui, o que causa encolhimento. '
 )
 
 criaCartao(
-  'animais'    
-   'O que torna um animal mamífero?'
+  'animais',
+   'O que torna um animal mamífero?',
    'Um mamífero é caracterizado principalmente por ter pelos no corpo e pela capacidade de produzir leite através de glândulas mamárias para alimentar seus filhotes.'
 )

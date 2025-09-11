e04ba5d983d96173121854ca94e9729311c9c293
victorbaqueta <victor.baqueta@escola.pr.gov.br>
2025-09-11 11:23:53 -0300

    n3

--- projeto 2/apps.js
+++ projeto 2/apps.js
@@ -1,6 +1,19 @@
-function criaCartao(categoria, pergunta, resposta) { 
+function criaCartao(categoria, pergunta, resposta) {
 
-    console.log(categoria ,pergunta, resposta)
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = `   
+     <div class="cartao__conteudo">
+                    <h3>Animais</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>Qual a principal diferença entre um Camelo e o Dromedário?</p>
+                    <div class="cartao__conteudo__resposta">
+                       <p>A principal diferença é o número de corcovas: o camelo tem duas corcovas, enquanto o dromedário tem apenas uma. </p>
+                    </div>
+                </div>`
 
 
+
+                container.appendChild(cartao)
 }

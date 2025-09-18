77e73538aad2194299b8646681f9b662bc88e23b
victorbaqueta <victor.baqueta@escola.pr.gov.br>
2025-09-18 11:31:29 -0300

    joao marcos da silva bezerra

--- projeto 2/perguntas.js
+++ projeto 2/perguntas.js
@@ -17,21 +17,21 @@ criaCartao(
 )
 
 criaCartao(
-   'animais',
-   'Qual a principal diferença entre um Camelo e o Dromedário?',
-   'A principal diferença é o número de corcovas: o camelo tem duas corcovas, enquanto o dromedário tem apenas uma.'
+   'alergicos',
+   'Quais são os principais alimentos alergênicos?',
+   'Entre os alimentos, os mais comuns são o leite, ovos, amendoim, nozes, soja, trigo, peixe e crustáceos. Outros alérgenos importantes incluem ácaros, pólen, pelos de animais, fungos, medicamentos, venenos de insetos (como vespas e abelhas), látex e cosméticos, que afetam o sistema respiratório ou a pele.'
 )
 
 criaCartao(
-  'pessoas',
-   'Por que os idosos diminuem de tamanho?',
-    'Os idosos perdem altura principalmente porque os discos intervertebrais da coluna se achatam e perdem fluidos, e a massa óssea e muscular diminui, o que causa encolhimento. '
+  'metal',
+   'Qual o único metal que é líquido em temperatura ambiente?',
+    'Mercúrio. '
 )
 
 criaCartao(
-  'animais',
-   'O que torna um animal mamífero?',
-   'Um mamífero é caracterizado principalmente por ter pelos no corpo e pela capacidade de produzir leite através de glândulas mamárias para alimentar seus filhotes.'
+  'Curiosidades',
+   'Qual país nasceu o escritor Franz Kafka?',
+   'Na atual República Tcheca.'
 )
 
 criaCartao(

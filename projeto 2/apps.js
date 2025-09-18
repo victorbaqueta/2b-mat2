bc070b9f61b42f756c572c7ae53c3c4b97f9ac4a
victorbaqueta <victor.baqueta@escola.pr.gov.br>
2025-09-18 10:57:07 -0300

    \z\

--- projeto 2/apps.js
+++ projeto 2/apps.js
@@ -5,12 +5,12 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao'
     cartao.innerHTML = `   
     <div class="cartao__conteudo">
-        <h3>Animais</h3>
+        <h3>${categoria}</h3>
         <div class="cartao__conteudo__pergunta">
-            <p>Qual a principal diferença entre um Camelo e o Dromedário?</p>
+            <p>${pergunta}</p>
         </div>
         <div class="cartao__conteudo__resposta">
-            <p>A principal diferença é o número de corcovas: o camelo tem duas corcovas, enquanto o dromedário tem apenas uma. </p>
+            <p>${resposta}</p>
         </div>
     </div>
     `
